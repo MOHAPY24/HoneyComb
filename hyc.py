7c1fcb7a1e7274d8e9035f3ebc2416a8cadfacf0
Mohammed Mahmoud <166178531+MOHAPY24@users.noreply.github.com>
2025-09-12 08:54:56 +0400

    Update hyc.py

--- hyc.py
+++ hyc.py
@@ -18,22 +18,22 @@ cptr = 0
 
 
 if __name__ == "__main__":
-    print("HoneyComb 0.0.1 BETA REPL, GPL 3.0 License Copyright Mohammed Abdelaal. \nPython 3.9 \n'--help' for more information.\n")
+    print("HoneyComb 1.0.0 BETA REPL, GPL 3.0 License Copyright Mohammed Abdelaal. \nPython 3.9 \n'--help' for more information.\n")
     while True:
         cmd = input(f"in [{cptr}] HoneyComb $$> ").strip()
         if cptr >= 20:
             if os.name == 'nt':
                 os.system("cls")
-                print("HoneyComb 0.0.1 BETA REPL, GPL 3.0 License Copyright Mohammed Abdelaal. \nPython 3.9 \n'--help' for more information.\n")
+                print("HoneyComb 1.0.0 BETA REPL, GPL 3.0 License Copyright Mohammed Abdelaal. \nPython 3.9 \n'--help' for more information.\n")
                 cptr = 0
                 pass
             else:
                 os.system("clear")
-                print("HoneyComb 0.0.1 BETA REPL, GPL 3.0 License Copyright Mohammed Abdelaal. \nPython 3.9 \n'--help' for more information.\n")
+                print("HoneyComb 1.0.0 BETA REPL, GPL 3.0 License Copyright Mohammed Abdelaal. \nPython 3.9 \n'--help' for more information.\n")
                 cptr = 0
                 pass
         if '--help' in cmd:
-            print("HoneyComb 0.0.1 \nHoneyComb is a Esoteric Programming Language (Esolang) made to have a tape like structure simular to a Honeycomb.\nHaving 3D movement throughout the Honeycomb expanding from up and down to entering a sector of a Honeycomb.\nWriten in Python 3.15.0, it is lightweight, memory safe, cleaned up automaticly by Python and the normal interperter itself and modular.")
+            print("HoneyComb 1.0.0 \nHoneyComb is a Esoteric Programming Language (Esolang) made to have a tape like structure simular to a Honeycomb.\nHaving 3D movement throughout the Honeycomb expanding from up and down to entering a sector of a Honeycomb.\nWriten in Python 3.15.0, it is lightweight, memory safe, cleaned up automaticly by Python and the normal interperter itself and modular.")
             pass
         elif "quit" in cmd:
             break
